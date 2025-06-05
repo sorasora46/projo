6fb401bbe0c2691f257a3d688837d92b6f33c14b
Sorrawit Kwanja <sorrawit02546@gmail.com>
2025-06-05 18:55:45 +0700

    chore: setup gorm

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
+	"github.com/sorasora46/projo/backend/internal/infras"
 	"github.com/sorasora46/projo/backend/internal/routers"
 )
 
@@ -23,13 +24,23 @@ func main() {
 	if ENV == "DEV" {
 		err = godotenv.Overload(".env.local")
 		if err != nil {
-			log.Printf("Warning: could not load .env.local: %v", err)
+			log.Printf("could not load .env.local: %v", err)
 		}
 	}
 
 	HOST := os.Getenv("HOST")
 	PORT := os.Getenv("PORT")
 	ADDR := fmt.Sprintf("%s:%s", HOST, PORT)
+
+	DB_HOST := os.Getenv("DB_HOST")
+	DB_PORT := os.Getenv("DB_PORT")
+	DB_USERNAME := os.Getenv("DB_USERNAME")
+	DB_PASSWORD := os.Getenv("DB_PASSWORD")
+	DB_NAME := os.Getenv("DB_NAME")
+	DSN := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_NAME,
+	)
+	infras.InitDB(DSN)
 
 	routers.InitRoutes(app)
 
