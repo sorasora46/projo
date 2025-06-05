2e02d26aa860b62907664e7aa26f90649b560e5f
Sorrawit Kwanja <sorrawit02546@gmail.com>
2025-06-05 18:40:04 +0700

    chore: setup env,addr

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -1,13 +1,37 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+	"github.com/sorasora46/projo/backend/internal/routers"
+)
 
 func main() {
 	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, fiber!")
-	})
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
 
-	app.Listen("127.0.0.1:8080")
+	ENV := os.Getenv("ENV")
+
+	if ENV == "DEV" {
+		err = godotenv.Overload(".env.local")
+		if err != nil {
+			log.Printf("Warning: could not load .env.local: %v", err)
+		}
+	}
+
+	HOST := os.Getenv("HOST")
+	PORT := os.Getenv("PORT")
+	ADDR := fmt.Sprintf("%s:%s", HOST, PORT)
+
+	routers.InitRoutes(app)
+
+	app.Listen(ADDR)
 }
