72e2e29ef0c4eeddac2136e3136259de335e873a
Sorrawit Kwanja <sorrawit02546@gmail.com>
2025-06-07 16:22:06 +0700

    chore: setup docker

--- backend/internal/usecases/user_usecase.go
+++ backend/internal/usecases/user_usecase.go
@@ -15,7 +15,44 @@ func NewUserUsercase(repo any) UserUsecase {
 	return &UserService{repo: repo}
 }
 
-func (u *UserService) Create() {}
-func (u *UserService) Get()    {}
-func (u *UserService) Update() {}
-func (u *UserService) Delete() {}
+func (u *UserService) CreateUser(req dtos.CreateUserReq) error {
+	// TODO: hash password
+	// TODO: add password to user entity
+	// hashe
+	newUser := entities.User{
+		Id:        uuid.NewString(),
+		FirstName: req.FirstName,
+		LastName:  req.LastName,
+		Username:  req.Username,
+		Email:     req.Email,
+		HashedPassword: ,
+	}
+	if err := u.repo.Create(newUser); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (u *UserService) GetByUsername(username string) (*dtos.UserDTO, error) {
+	user, err := u.repo.GetByUsername(username)
+	if err != nil {
+		return nil, err
+	}
+
+	dto := dtos.UserDTO{
+		Id:        user.Id,
+		FirstName: user.FirstName,
+		LastName:  user.LastName,
+		Username:  user.Username,
+		Email:     user.Email,
+	}
+
+	return &dto, nil
+}
+
+func (u *UserService) DeleteByUsername(username string) error {
+	if err := u.repo.DeleteByUsername(username); err != nil {
+		return err
+	}
+	return nil
+}
