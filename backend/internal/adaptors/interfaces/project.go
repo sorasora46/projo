02155088c68625c3cf40e39f9c86ababeaae3b52
Sorrawit Kwanja <sorrawit02546@gmail.com>
2025-06-09 00:57:38 +0700

    feat: init project repo

---
+++ backend/internal/adaptors/interfaces/project.go
@@ -0,0 +1,8 @@
+package interfaces
+
+import "github.com/sorasora46/projo/backend/internal/entities"
+
+type ProjectRepository interface {
+	Create(newProject entities.Project) error
+	GetByProjectId(projectId string) (*entities.Project, error)
+}
