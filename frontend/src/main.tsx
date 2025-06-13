344bb2a681804541f6957d1e497221406e5f6f3f
Sorrawit Kwanja <sorrawit02546@gmail.com>
2025-06-13 17:52:47 +0700

    feat: init basic routing

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
+import { BrowserRouter, Route } from "react-router";
+import Home from "./pages/Home.tsx";
 import "./index.css";
+import { Routes } from "react-router";
+import Login from "./pages/Login.tsx";
+import Register from "./pages/Register.tsx";
+import NotFound from "./pages/NotFound.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 );
