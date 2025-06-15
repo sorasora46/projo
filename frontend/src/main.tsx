c312ba30c3c8436177e4530eb48c50f028eb449c
Sorrawit Kwanja <sorrawit02546@gmail.com>
2025-06-15 15:04:26 +0700

    feat: move routing to App.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,22 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Route } from "react-router";
-import Home from "./pages/Home.tsx";
+import App from "./App";
 import "./index.css";
-import { Routes } from "react-router";
-import Login from "./pages/Login.tsx";
-import Register from "./pages/Register.tsx";
-import NotFound from "./pages/NotFound.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/" element={<Home />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </StrictMode>,
 );
