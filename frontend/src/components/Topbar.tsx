fe598cd94c60ba33a6442702e78970055156d1f5
Sorrawit Kwanja <sorrawit02546@gmail.com>
2025-06-17 11:58:29 +0700

    chore: extract topbar to component

---
+++ frontend/src/components/Topbar.tsx
@@ -0,0 +1,78 @@
+import { useState } from "react";
+import { CgProfile } from "react-icons/cg";
+import { HiBell } from "react-icons/hi";
+import {
+  HiCalendarDays,
+  HiOutlineBell,
+  HiOutlineCalendarDays,
+} from "react-icons/hi2";
+import { MdLogout } from "react-icons/md";
+import { RiArrowDownSLine, RiArrowUpSLine } from "react-icons/ri";
+import { Link } from "react-router";
+
+const Topbar = () => {
+  const [isCalendarHover, setIsCalendarHover] = useState(false);
+  const [isBellHover, setIsBellHover] = useState(false);
+  const [isProfileOpen, setIsProfileOpen] = useState(false);
+  return (
+    <header className="w-full flex justify-between items-center bg-blue-500 p-4 py-2 pe-3">
+      <h1>Hello, firstName lastName</h1>
+      <div className="flex items-center gap-2">
+        <ul className="flex items-center gap-5 text-lg">
+          <li>
+            <Link
+              to=""
+              onMouseEnter={() => setIsCalendarHover(true)}
+              onMouseLeave={() => setIsCalendarHover(false)}
+            >
+              {isCalendarHover ? <HiCalendarDays /> : <HiOutlineCalendarDays />}
+            </Link>
+          </li>
+          <li>
+            <Link
+              to=""
+              onMouseEnter={() => setIsBellHover(true)}
+              onMouseLeave={() => setIsBellHover(false)}
+            >
+              {isBellHover ? <HiBell /> : <HiOutlineBell />}
+            </Link>
+          </li>
+        </ul>
+        <button
+          className="btn btn-link text-xl"
+          popoverTarget="popover-1"
+          style={{ anchorName: "--anchor-1" } as React.CSSProperties}
+          onClick={() => setIsProfileOpen((prev) => !prev)}
+          onBlur={() => setIsProfileOpen(false)}
+        >
+          <img
+            className="mask mask-circle w-8 h-8"
+            src="https://img.daisyui.com/images/stock/photo-1567653418876-5bb0e566e1c2.webp"
+          />
+          {isProfileOpen ? <RiArrowUpSLine /> : <RiArrowDownSLine />}
+        </button>
+        <ul
+          className="dropdown menu bg-base-100 shadow-sm p-0 [&_li>*]:rounded-none [&_li>*]:px-5"
+          popover="auto"
+          id="popover-1"
+          style={{ positionAnchor: "--anchor-1" } as React.CSSProperties}
+        >
+          <li>
+            <Link to="/profile">
+              <CgProfile />
+              Profile
+            </Link>
+          </li>
+          <li>
+            <button>
+              <MdLogout />
+              Logout
+            </button>
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
+};
+
+export default Topbar;
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,20 +1,6 @@
-import { useState } from "react";
-import { CgProfile } from "react-icons/cg";
-import { HiBell } from "react-icons/hi";
-import {
-  HiCalendarDays,
-  HiOutlineBell,
-  HiOutlineCalendarDays,
-} from "react-icons/hi2";
-import { MdLogout } from "react-icons/md";
-import { RiArrowDownSLine, RiArrowUpSLine } from "react-icons/ri";
-import { Link } from "react-router";
+import Topbar from "../components/Topbar";
 
 const Home = () => {
-  const [isCalendarHover, setIsCalendarHover] = useState(false);
-  const [isBellHover, setIsBellHover] = useState(false);
-  const [isProfileOpen, setIsProfileOpen] = useState(false);
-
   return (
     <div className="h-dvh w-full flex">
       <aside className="h-full bg-blue-500 p-4">
@@ -22,67 +8,7 @@ const Home = () => {
         <nav></nav>
       </aside>
       <div className="w-full flex flex-col">
-        <header className="w-full flex justify-between items-center bg-blue-500 p-4 pe-3">
-          <h1>Hello, firstName lastName</h1>
-          <div className="flex items-center gap-2">
-            <ul className="flex items-center gap-5 text-lg">
-              <li>
-                <Link
-                  to=""
-                  onMouseEnter={() => setIsCalendarHover(true)}
-                  onMouseLeave={() => setIsCalendarHover(false)}
-                >
-                  {isCalendarHover ? (
-                    <HiCalendarDays />
-                  ) : (
-                    <HiOutlineCalendarDays />
-                  )}
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to=""
-                  onMouseEnter={() => setIsBellHover(true)}
-                  onMouseLeave={() => setIsBellHover(false)}
-                >
-                  {isBellHover ? <HiBell /> : <HiOutlineBell />}
-                </Link>
-              </li>
-            </ul>
-            <button
-              className="btn btn-link text-xl"
-              popoverTarget="popover-1"
-              style={{ anchorName: "--anchor-1" } as React.CSSProperties}
-              onClick={() => setIsProfileOpen((prev) => !prev)}
-              onBlur={() => setIsProfileOpen(false)}
-            >
-              <img
-                className="mask mask-circle w-8 h-8"
-                src="https://img.daisyui.com/images/stock/photo-1567653418876-5bb0e566e1c2.webp"
-              />
-              {isProfileOpen ? <RiArrowUpSLine /> : <RiArrowDownSLine />}
-            </button>
-            <ul
-              className="dropdown menu bg-base-100 shadow-sm p-0 [&_li>*]:rounded-none [&_li>*]:px-5"
-              popover="auto"
-              id="popover-1"
-              style={{ positionAnchor: "--anchor-1" } as React.CSSProperties}
-            >
-              <li>
-                <Link to="/profile">
-                  <CgProfile />
-                  Profile
-                </Link>
-              </li>
-              <li>
-                <button>
-                  <MdLogout />
-                  Logout
-                </button>
-              </li>
-            </ul>
-          </div>
-        </header>
+        <Topbar />
         <main className="flex-1 bg-gray-100 p-4">
           <p>Main content area</p>
         </main>
