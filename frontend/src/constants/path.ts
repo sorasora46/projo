9b000bb086930b682be768499daf1f4f33ca694a
Sorrawit Kwanja <sorrawit02546@gmail.com>
2025-06-17 12:37:32 +0700

    chore: refactor paths to constant

---
+++ frontend/src/constants/path.ts
@@ -0,0 +1,9 @@
+export const ProjoPath = {
+  HOME: "/",
+  ARCHIVED: "/archived",
+  DELETED: "/deleted",
+  PROFILE: "/profile",
+  LOGIN: "/login",
+  REGISTER: "/register",
+  FORGOT_PASSWORD: "/forgot-password",
+};
